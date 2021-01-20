48de8ba96f444e16b78fe886306213d429880bcc
jingjing20 <1269928993@qq.com>
2021-01-20 11:16:15 +0800

    模块热更新

---
+++ HMR/src/index.js
@@ -0,0 +1,12 @@
+import counter from './counter';
+import number from './number';
+
+counter();
+number();
+
+if (module.hot) {
+	module.hot.accept('./number', () => {
+		document.body.removeChild(document.getElementById('number'));
+		number();
+	});
+}
