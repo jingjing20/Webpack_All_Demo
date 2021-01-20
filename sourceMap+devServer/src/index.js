c14778e0a4d807b4fa5e3dbfdf2761dcf6ccc443
jingjing20 <1269928993@qq.com>
2021-01-20 11:16:45 +0800

    rename

---
+++ sourceMap+devServer/src/index.js
@@ -0,0 +1,9 @@
+const root = document.getElementById('root');
+const dom = document.createElement('div');
+dom.innerText = '今天天气真好啊！';
+dom.style.fontSize = '36px';
+dom.style.color = 'red';
+
+root.appendChild(dom);
+
+console.log('jingjing'); //jing-log
