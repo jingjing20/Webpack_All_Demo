7e600498fab00acb22fcfa038dbb4680a0a055e0
jingjing20 <1269928993@qq.com>
2021-01-25 10:44:22 +0800

    webapck 基础完结

--- code-spliting/src/index.js
+++ code-spliting/src/index.js
@@ -12,5 +12,5 @@ getComponent().then((element) => {
 
 // 代码分割，和webpack无关
 // webpack中实现代码分割，两种方式
-// 1. 同步代码： 只需要在webpack.common.js中做optimization的配置即可
+// 1. 同步代码： 只需要在webpack.common.js中做 optimization 的配置即可
 // 2. 异步代码(import): 异步代码，无需做任何配置，会自动进行代码分割，放置到新的文件中
