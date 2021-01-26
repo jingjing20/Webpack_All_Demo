583a449ad3f0d0e8decde785b1ef55f1ab6a137a
jingjing20 <1269928993@qq.com>
2021-01-26 11:55:48 +0800

    babel 自己转换浏览器能执行的代码完成

--- bundler/bundler.js
+++ bundler/bundler.js
@@ -36,6 +36,7 @@ const moduleAnalyser = (filename) => {
 	};
 };
 
+// 解析所有模块的依赖关系，得到一个依赖图谱
 const makeDependenciesGraph = (entry) => {
 	// 调用 moduleAnalyser 函数分析出入口文件的依赖关系
 	const entryModule = moduleAnalyser(entry);
@@ -60,5 +61,30 @@ const makeDependenciesGraph = (entry) => {
 	return graph;
 };
 
-const moduleInfo = makeDependenciesGraph('./src/index.js');
-console.log(moduleInfo); //jing-log
+// 生成浏览器能执行的代码
+const generateCode = (entry) => {
+	const graph = JSON.stringify(makeDependenciesGraph(entry));
+	return `
+        (function(graph){
+            function require(module) {
+                function localRequire(relativePath) {
+                    return require(graph[module].dependencies[relativePath])
+                }
+                const exports = {};
+                (function(require, exports, code) {
+                    eval(code)
+                })(localRequire, exports, graph[module].code)
+                return exports;
+            }
+            require('${entry}')
+        })(${graph})
+    `;
+};
+
+const code = generateCode('./src/index.js');
+console.log(code); //jing-log
+
+// 生成代码的时候注意点：
+// 1、因为 makeDependenciesGraph 得到的是一个依赖对象，是一个对象，如果放在一段字符串里面，得到的会是 [Object, Object]。所以得先转成字符串。
+// 2、返回的是一段 js 代码，所以返回的是一段字符串。在浏览器执行的代码为了避免全局变量污染一般都会放在一个闭包里面执行。
+// 3、依赖对象里面含有 require() 和 exports 对象，我们要自己定义他们。
